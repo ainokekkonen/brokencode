c78345f62283083835d1661ea66d42ec33f739d6
Aino Kekkonen <ainokekkonen@hotmail.com>
2025-11-29 13:16:49 +0200

    added glitch

--- world.gd
+++ world.gd
@@ -1,12 +1,11 @@
 extends Node2D
 
 
-func _ready():
-	# Load and instance the fade transition scene
-	var fade_scene = preload("res://fadetransition.tscn").instantiate()
-	get_tree().root.add_child(fade_scene)  # Add it on top of everything
-	fade_scene.z_index = 9999  # Ensure it's above all other nodes
 
-	# Get the AnimationPlayer inside the fade scene
-	var anim_player = fade_scene.get_node("AnimationPlayer")  # Adjust if needed
+func _ready():
+	var fade_scene = preload("res://fadetransition.tscn").instantiate()
+	get_tree().root.add_child(fade_scene)  # CanvasLayer ignores camera
+	fade_scene.z_index = 9999
+
+	var anim_player = fade_scene.get_node("AnimationPlayer")
 	anim_player.play("fade_out")
