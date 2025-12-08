895138017e82aeda105f376d6d39b6bc9db4542e
Saana Parkkila <parkkila.saana@gmail.com>
2025-12-08 16:31:59 +0200

    vihuu

--- enemy_1.gd
+++ enemy_1.gd
@@ -22,6 +22,17 @@ var is_roaming: bool = true
 var player: CharacterBody2D
 var player_in_area = false
 
+var attack_cooldown = 1.5
+var can_attack = true
+
+const jump_velocity = -400
+var is_jumping: bool = false
+var just_landed: bool = false
+var was_on_floor: bool = false
+
+func _ready():
+	$AnimatedSprite2D.animation_finished.connect(_on_anim_finished)
+
 func _process(delta):
 	if !is_on_floor():
 		velocity.y += gravity * delta
@@ -36,10 +47,16 @@ func move(delta):
 	if !dead:
 		if !is_enemy_chase:
 			velocity += dir * speed * delta
+			if velocity.x != 0:
+				dir.x = sign(player.position.x - position.x)
 		elif is_enemy_chase and !taking_damage:
-			var dir_to_player = position.direction_to(player.position) * speed
+			var dir_to_player = (player.position - position).normalized() * speed
 			velocity.x = dir_to_player.x
-			dir.x = abs(velocity.x) / velocity.x
+			dir.x = sign(player.position.x - position.x)
+			if player.position.y < position.y - 150 and is_on_floor():
+				velocity.y = jump_velocity
+				is_jumping = true
+				just_landed = false
 		elif taking_damage:
 			var knockback_dir = position.direction_to(player.position) * knockback_force
 			velocity.x = knockback_dir.x
@@ -50,11 +67,28 @@ func move(delta):
 func handle_animation():
 	var anim_sprite = $AnimatedSprite2D
 	if !dead and !taking_damage and !is_dealing_damage:
-		anim_sprite.play("Walk")
-		if dir.x == -1:
-			anim_sprite.flip_h = true
-		elif dir.x == 1:
-			anim_sprite.flip_h = false
+		if is_jumping:
+			anim_sprite.play("JumpStart")
+			await get_tree().create_timer(0.1).timeout
+			is_jumping = false
+			anim_sprite.flip_h = (dir.x < 0)
+			return
+		if just_landed:
+			anim_sprite.play("Land")
+			anim_sprite.flip_h = (dir.x < 0)
+			return
+		if !is_on_floor():
+			if velocity.y < 0:
+				anim_sprite.play("JumpLoop")
+				anim_sprite.flip_h = (dir.x < 0)
+				return
+			elif velocity.y > 0:
+				anim_sprite.play("Fall")
+				anim_sprite.flip_h = (dir.x < 0)
+				return
+		else:
+			anim_sprite.play("Walk")
+			anim_sprite.flip_h = (dir.x < 0)
 	elif !dead and taking_damage and !is_dealing_damage:
 		anim_sprite.play("Damage")
 		await get_tree().create_timer(0.8).timeout
@@ -68,7 +102,17 @@ func handle_animation():
 func handle_death():
 	self.queue_free()
 
-
+func _on_anim_finished():
+	var anim = $AnimatedSprite2D.animation
+	if anim == "JumpStart":
+		is_jumping = false
+		if !is_on_floor() and velocity.y < 0:
+			$AnimatedSprite2D.play("JumpLoop")
+		elif !is_on_floor():
+			$AnimatedSprite2D.play("Fall")
+	elif anim == "Land":
+		just_landed = false
+		$AnimatedSprite2D.play("Walk")
 
 func _on_direction_timer_timeout() -> void:
 	$DirectionTimer.wait_time = choose([1.5,2.0,2.5])
@@ -86,3 +130,39 @@ func take_damage(damage):
 	if health <= health_min:
 		health = health_min
 		dead = true
+
+func _on_detection_area_body_entered(body):
+	print("DetectionArea entered by:", body.name)
+	if body.is_in_group("Player"):
+		player = body
+		is_enemy_chase = true
+		player_in_area = true
+		attack()
+
+func _on_detection_area_body_exited(body):
+	if body.is_in_group("Player"):
+		is_enemy_chase = false
+		player_in_area = false
+
+func attack():
+	if player_in_area and !dead and !taking_damage and can_attack:
+		can_attack = false
+		is_dealing_damage = true
+		$AnimatedSprite2D.play("Attack")
+		if player.has_method("take_damage"):
+			player.take_damage(damage_to_deal)
+		await get_tree().create_timer(0.5).timeout
+		is_dealing_damage = false
+		await get_tree().create_timer(attack_cooldown).timeout
+		can_attack = true
+
+func _on_hitbox_area_entered(area):
+	if area.name == "PlayerAttack":
+		take_damage(20)
+
+func _physics_process(delta):
+	if !was_on_floor and is_on_floor():
+		just_landed = true
+	was_on_floor = is_on_floor()
+	handle_animation()
+		
