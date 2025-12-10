38ddfd324901f932608d88151b0174e3d73e1df3
Saana Parkkila <parkkila.saana@gmail.com>
2025-12-10 14:08:58 +0200

    vihuu

--- enemy_1.gd
+++ enemy_1.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 class_name Enemy1
 @onready var anim: AnimationPlayer = $AnimationPlayer
-@onready var animspr: AnimatedSprite2D = $AnimatedSprite2D 
+@onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D 
 
 const ENEMY_HURT_SPRITE := "damage"  # AnimatedSprite2D / SpriteFrames
 const ENEMY_HURT_AP     := "hurt"    # AnimationPlayer
@@ -36,8 +36,6 @@ var is_jumping: bool = false
 var just_landed: bool = false
 var was_on_floor: bool = false
 
-func _ready():
-	$AnimatedSprite2D.animation_finished.connect(_on_AnimatedSprite2D_animation_finished)
 
 func _process(delta):
 	if !is_on_floor():
@@ -49,6 +47,10 @@ func _process(delta):
 	move_and_slide()
 
 func move(delta):
+	if is_dealing_damage:
+		velocity.x = 0
+		velocity.y = 0
+		return
 	if !dead:
 		if !is_enemy_chase:
 			velocity += dir * speed * delta
@@ -70,7 +72,10 @@ func move(delta):
 		velocity.x = 0
 
 func handle_animation():
-	var anim_sprite = $AnimatedSprite2D
+	if is_dealing_damage:
+		print("soitetaan attack animaatio")
+		anim_sprite.play("Attack")
+		return
 	if !dead and !taking_damage and !is_dealing_damage:
 		if is_jumping:
 			anim_sprite.play("JumpStart")
@@ -109,15 +114,25 @@ func handle_death():
 
 func _on_anim_finished():
 	var anim = $AnimatedSprite2D.animation
+	
 	if anim == "JumpStart":
 		is_jumping = false
 		if !is_on_floor() and velocity.y < 0:
 			$AnimatedSprite2D.play("JumpLoop")
 		elif !is_on_floor():
 			$AnimatedSprite2D.play("Fall")
+	
 	elif anim == "Land":
 		just_landed = false
 		$AnimatedSprite2D.play("Walk")
+	
+	elif anim == "Attack":
+		is_dealing_damage = false
+		if !dead:
+			$AnimatedSprite2D.play("Walk")
+	
+	elif anim == "Death":
+		handle_death()
 
 func _on_direction_timer_timeout() -> void:
 	$DirectionTimer.wait_time = choose([1.5,2.0,2.5])
@@ -129,21 +144,16 @@ func choose(array):
 	array.shuffle()
 	return array.front()
 
-func take_damage(damage):
+func take_damage(damage: int) -> void:
 	health -= damage
 	taking_damage = true
-	
-	$AnimatedSprite2D.play(ENEMY_HURT_SPRITE)
-	anim.play(ENEMY_HURT_AP)
-	
+	$AnimatedSprite2D.play("Damage")
 	if health <= health_min:
 		health = health_min
 		dead = true
 
-
-func _on_AnimatedSprite2D_animation_finished() -> void:
-	if $AnimatedSprite2D.animation == ENEMY_HURT_SPRITE:
-		taking_damage = false
+func _ready():
+	$AnimatedSprite2D.animation_finished.connect(_on_anim_finished)
 
 
 func start_hurt() -> void:
@@ -159,7 +169,6 @@ func _on_detection_area_body_entered(body):
 		player = body
 		is_enemy_chase = true
 		player_in_area = true
-		attack()
 
 func _on_detection_area_body_exited(body):
 	if body.is_in_group("Player"):
@@ -170,21 +179,44 @@ func attack():
 	if player_in_area and !dead and !taking_damage and can_attack:
 		can_attack = false
 		is_dealing_damage = true
-		$AnimatedSprite2D.play("Attack")
-		if player.has_method("take_damage"):
-			player.take_damage(damage_to_deal)
-		await get_tree().create_timer(0.5).timeout
-		is_dealing_damage = false
+		anim_sprite.play("Attack")
+		
+		
+		var attack_duration = 0.4
+		var target_pos = player.position
+		var tween = create_tween()
+		tween.tween_property(self, "position", target_pos, attack_duration)
+		
+		$AttackHitbox.monitoring = true
+		
+		await get_tree().create_timer(attack_duration).timeout
+		$AttackHitbox.monitoring = false
+		
 		await get_tree().create_timer(attack_cooldown).timeout
 		can_attack = true
 
-func _on_hitbox_area_entered(area):
+
+func _on_Hitbox_area_entered(area):
 	if area.name == "PlayerAttack":
 		take_damage(20)
+
 
 func _physics_process(delta):
 	if !was_on_floor and is_on_floor():
 		just_landed = true
 	was_on_floor = is_on_floor()
 	handle_animation()
-		
+	if player_in_area and can_attack and !dead and !taking_damage:
+		attack()
+
+
+func  _on_attack_hitbox_area_entered(area: Area2D) -> void:
+	print("AttackHitbox osui:", area.name)
+	if area.is_in_group("Player"):
+		is_dealing_damage = true
+		if area.has_method("take_damage"):
+			area.take_damage(damage_to_deal)
+		can_attack = false
+		await get_tree().create_timer(attack_cooldown).timeout
+		can_attack = true
+		is_dealing_damage = false
